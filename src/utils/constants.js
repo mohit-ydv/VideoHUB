3eb9caaf4ac4d06a165086a8d0c1092a2a1cb6c4
Mohit Yadav <100510948+mohit-ydv@users.noreply.github.com>
2024-04-13 16:28:58 +0530

    Updated README.md

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = "AIzaSyCvI_ejr7D3ioFFaoX5GPAyHUA_ouIauM4";
+const GOOGLE_API_KEY = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
 export const YOUTUBE_VIDEOS_API =
   "https://content-youtube.googleapis.com/youtube/v3/videos?part=statistics%2CcontentDetails%2Csnippet&maxResults=50&chart=mostPopular&key=" +
   GOOGLE_API_KEY;
